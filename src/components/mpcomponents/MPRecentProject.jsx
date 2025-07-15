0c2c0a80a145aea54378dcf239d942c0afd9dc3a
dogsalt_ <jpstennes@gmail.com>
2025-07-15 15:08:04 -0600

    mp recent project component elements relayout

--- src/components/mpcomponents/MPRecentProject.jsx
+++ src/components/mpcomponents/MPRecentProject.jsx
@@ -5,20 +5,16 @@ export default function MPRecentProject({ project }) {
         <section className='mp_recent_project_container'>
             <header>
                 <h1>Most Recently..</h1>
-                <h3>{project.recentProjectTitle}</h3>
                 <div className='line_divider'></div>
             </header>
 
-            <article>
-                <p>{project.articleParagraph}</p>
-                <aside>{project.articleAside}</aside>
-            </article>
-
             <figure>
                 <img src={project.GalleryImgSrc1} alt={`${project.recentProjectTitle} gallery photo 1`} />
-                <img src={project.GalleryImgSrc2} alt={`${project.recentProjectTitle} gallery photo 2`} />
-                <img src={project.GalleryImgSrc3} alt={`${project.recentProjectTitle} gallery photo 3`} />
-                <figcaption>{project.galleryFigcaption}</figcaption>
+                <figcaption>
+                    <h3>{project.recentProjectTitle}</h3>
+                    <p>{project.articleParagraph}</p>
+                </figcaption>
+                <aside>{project.articleAside}</aside>
             </figure>
         </section>
     )
