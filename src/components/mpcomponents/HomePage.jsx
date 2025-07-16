d63ebceb69725a81109f9b3769b8d928210d4897
dogsalt_ <jpstennes@gmail.com>
2025-07-16 15:08:13 -0600

    almost finished with upcoming projects section layout also added images

--- src/components/mpcomponents/HomePage.jsx
+++ src/components/mpcomponents/HomePage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import tomePromptIcon from '../../assets/tome_prompt_icon.png';
 
 // Components
 import MPRecentProject from './MPRecentProject';
 import MPUpcomingProjects from './MPUpcomingProjects';
 import MPLatestDecryptiumEntry from './MPLatestDecryptiumEntry';
+
+// File Imports
+import tomePromptIcon from '../../assets/tome_prompt_icon.png';
+import BoneSaltIcon from '../../assets/Bonesalt_icon.png';
+import theWrappedOneIcon from '../../assets/the_wrapped_one_icon.png'
+import tarnishedEpIcon from '../../assets/tarnished_ep_icon.png';
 
 export default function HomePage() {
     
@@ -29,19 +34,19 @@ export default function HomePage() {
                 projects={
                     [
                     {
-                        image: 'img1.png', 
+                        image: BoneSaltIcon, 
                         title: 'BoneSalt (pico8 game)', 
                         description: 'BoneSalt is a retro dungeon crawler survival game built in the fantasy console game engine, Pico 8', 
                         aside: 'this is one of the earliest tales in the dogsalt universe'
                     },
                     {
-                        image: 'img2.png',
+                        image: theWrappedOneIcon,
                         title: 'The_Wrapped_One.mov (Short Film)', 
                         description: 'The_Wrapped_One.mov is a surreal and bizzare analog horror short film of a dialogue between young Jacky and Reykbein.', 
                         aside: 'this film comes long long after the ending events of BoneSalt'
                     },
                     {
-                        image: 'img3.png', 
+                        image: tarnishedEpIcon, 
                         title: 'Tarnished (EP)', 
                         description: 'Tarnished is a shoegaze inspired by some of my favorite media as well as a very challenging time in my life.', 
                         aside: 'I began working on this album around the same time as building the first model for dogsalt'
