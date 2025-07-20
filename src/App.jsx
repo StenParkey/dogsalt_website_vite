755dede883a01ed5e749ce5aa7fe93d8dc24b82d
dogsalt_ <jpstennes@gmail.com>
2025-07-20 17:42:04 -0600

    added new navbuttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 // Components
 import PageHeader from './components/universal_components/PageHeader';
 import PageFooter from './components/universal_components/PageFooter';
+// Main Components
 import HomePage from './components/mpcomponents/HomePage';
 import DecryptiumPage from './components/decryptium/DecryptiumPage';
 
