28580eff411417d8b3998aec203e6753e3b3ba9f
dogsalt_ <jpstennes@gmail.com>
2025-07-21 23:30:11 -0600

    finished project classes

--- src/models/projectClass.js
+++ src/models/projectClass.js
@@ -1,3 +1,4 @@
+// Master Project class
 class Project {
     constructor({ projectName, projectType, projectIcon, projectDescription }) {
         this.projectName = projectName;
@@ -10,7 +11,7 @@ class Project {
         this.contentGallery = null;
     }
 
-    completeProject() {
+    completeProject({ article, aside, contentGallery }) {
         this.isCompleted = true;
         this.article = article;
         this.aside = aside;
@@ -18,40 +19,70 @@ class Project {
     }
 }
 
-
+// Music Project Class
 class MusicProject extends Project {
-    constructor({ ...rest }) {
+    constructor({ teaserAudio, ...rest }) {
         super({...rest, type: 'music'});
+        this.teaserAudio = teaserAudio;
+        this.albumArt = null;
+        this.tracks = null;
     }
     
-    completeProject() {
-        super(completeProject());
-        tracks
+    completeProject({ albumArt, tracks, ...rest }) {
+        super(rest);
+        this.albumArt = albumArt;
+        this.tracks = tracks;
     }
 }
 
+// Film Project Class 
 class FilmProject extends Project {
-    constructor({ medium, length, ...rest }) {
+    constructor({ medium, filmPoster, ...rest }) {
         super({...rest, type: 'film'});
         this.medium = medium;
+        this.filmPoster = filmPoster;
+        this.length = null;
+        this.film = null;
+    }
+    
+    completeProject({ length, film, ...rest }) {
+        super(rest);
         this.length = length;
+        this.film = film;
     }
 }
 
-
+// Game Project Class
 class GameProject extends Project {
-    constructor({ gameEngine, availableOn, ...rest }) {
+    constructor({ gameEngine, gameGenre, ...rest }) {
         super({...rest, type: 'game'});
         this.gameEngine = gameEngine;
+        this.gameGenre = gameGenre;
+        this.availableOn = null;
+        this.game = null;
+    }
+
+    completeProject({ availableOn, game, ...rest }) {
+        super(rest);
         this.availableOn = availableOn;
+        this.game = game;
     }
 }
 
-
+// App Project Class 
 class AppProject extends Project {
-    constructor({ madeWith, availableOn, ...rest }) {
+    constructor({ madeWith, ...rest }) {
         super({...rest, type: 'app'});
         this.madeWith = madeWith;
+        this.availableOn = null;
+        this.app = null;
+    }
+
+    completeProject({ availableOn, app, ...rest}) {
+        super(rest); 
         this.availableOn = availableOn;
+        this.app = app;
     }
 }
+
+export { Project, MusicProject, FilmProject, GameProject, AppProject };
