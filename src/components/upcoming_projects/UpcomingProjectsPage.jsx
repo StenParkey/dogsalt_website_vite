97cb25cfb3f031ed68aac68c245d13f029b81258
dogsalt_ <jpstennes@gmail.com>
2025-07-23 19:36:26 -0600

    halted on upcoming projects page, might need some more thinking and planning

--- src/components/upcoming_projects/UpcomingProjectsPage.jsx
+++ src/components/upcoming_projects/UpcomingProjectsPage.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
 
+
+// This all might be fucked, i might want to refactor each gallery as a seperate component that can be rendered, and if on either upcoming projects
+// or recent projects, it will display the corresponding content dynamically in reusable components
+
+
 export default function UpcomingProjectsPage() {
     return (
         <>
-        
+            <section>
+                <header>
+                    <h1>Coming Soon...</h1>
+                    <div className='line_divider'></div>
+                </header>
+
+                <figure>
+                    <article>
+                        <figcaption>Some fuckass project idk</figcaption>
+                        <p>this is the description of the fuckass project </p>
+                    </article>
+
+                    <img src={`placeholder`} />
+                </figure>
+            </section>
+
+            <div>
+                <section>
+                    <h2>Music</h2>
+
+                    <ul>
+
+                    </ul>
+                </section>
+
+                <section>
+                    <h2>Film</h2>
+
+                    <ul>
+
+                    </ul>
+                </section>
+
+                <section>
+                    <h2>Games</h2>
+
+                    <ul>
+
+                    </ul>
+                </section>
+
+                <section>
+                    <h2>Apps</h2>
+
+                    <ul>
+
+                    </ul>
+                </section>
+            </div>
         </>
     )
 };
