8858e6a5885b3e1fbd1decb7d6740accc4c518f9
dogsalt_ <jpstennes@gmail.com>
2025-07-23 19:52:44 -0600

    setup initial community data object for community page

---
+++ src/models/community/communityData.js
@@ -0,0 +1,59 @@
+const communityData = {
+    socialmedia: {
+            youtube: {
+                title: 'dogsalt_ ',
+                link: 'youtube.com'
+            },
+
+            twitch: {
+                title: 'dogsalt_',
+                link: 'twitch.tv'
+            },
+            
+            tiktok: {
+                title: 'dogsalt_ ',
+                link: 'tiktok.com'
+            },
+
+            instagram: {
+                title: 'dogsalt_ ig',
+                link: 'instagram.com'
+            },
+
+            twitter: {
+                title: 'dogsalt_',
+                link: 'StupidElonX.com'
+            }
+    },
+
+    music: {
+            spotify: {
+                title: 'dogsalt_ ',
+                link: 'spotify.com'
+            },
+
+            applemusic: {
+                title: 'dogsalt_ ',
+                link: 'applemusic.com'
+            },
+
+            soundcloud: {
+                title: 'dogsalt_ soundcloud',
+                link: 'soundcloud.com'
+            },
+
+            bandcamp: {
+                title: 'dogsalt_ bandcamp',
+                link: 'bandcamp.com'
+            }
+    },
+
+    coding: {
+        itchIO: {
+            title: 'dogsalt itchIO',
+            link: 'itch.io'
+        }
+    }
+}
+
+export { communityData };
