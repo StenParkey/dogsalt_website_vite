fa4b1e43002ff1e04eb536415f5517e421e2de44
dogsalt_ <jpstennes@gmail.com>
2025-07-23 19:57:58 -0600

    setup prank shop page component

---
+++ src/components/shop/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function ShopPage() {
+    return (
+        <section>
+            <header>
+                <h1>The Shop</h1>
+                <div className='line_divider'></div>
+            </header>
+
+            <section>
+                <span>Haha welcome! Theres nothing for sale rn, but maybe there will be in the future?</span>
+            </section>
+        </section>
+    )
+};
