92ff611342ca020284b3a608c0dd41b11e0cd3af
dogsalt_ <jpstennes@gmail.com>
2025-07-26 12:03:00 -0600

    populated decryptium entries with some placeholder stuff

--- src/models/decryptium/moldflower_tomes/moldFlowerTomes.js
+++ src/models/decryptium/moldflower_tomes/moldFlowerTomes.js
@@ -5,10 +5,51 @@ const tome1 = new MoldFlowerTome({
     tomeData: 'I was ronnin around an mai waif, mai waife she sayd to me she said, yOu SpEnDin To Moch MoenY HAHhA.. so now i am waiting for her outside the tj mack'
 });
 
+const tome2 = new MoldFlowerTome({
+    tomeName: 'Tome 2 - Rain',
+    tomeData: 'Aight so i really got no fuckin clue what to write in these. some are probably gonna be filled with lorem ipsums and shit. Idk my g'
+});
+
+const tome3 = new MoldFlowerTome({
+    tomeName: 'Tome 3 - Exile',
+    tomeData: 'I am currently reading 3 different manga series. Dai Dark by Q Hayashida, DanDaDan by Yukinobu Tatsu, and finally i think my now favorite manga ever Black Lagoon by Rei Hiroe.'
+});
+
+const tome4 = new MoldFlowerTome({
+    tomeName: 'Tome 4 - Surmise', 
+    tomeData: 'AHHHHHHHH AHHHHHHHHHHHHHH AHHHH AHHHHHHHHHHHHHHH AHHHHHH HAHHAHHHH AHHHHHHHHHHH AHAHAH HHHAHHHHHHHHHHAHHHHHH AHHHHHHHHH.'
+});
+
+const tome5 = new MoldFlowerTome({
+    tomeName: 'Tome 5 - Petrified',
+    tomeData: 'wtf gurl i dont even know. Im goin crazyyyyyy...'
+});
+
+const tome6 = new MoldFlowerTome({
+    tomeName: 'Tome 6 - Lucid',
+    tomeData: 'fart fart fart fart haha. Im running out of ideas rapidly for content. All of this will be replaced later.'
+});
+
+const tome7 = new MoldFlowerTome({
+    tomeName: 'Tome 7 - Cowl', 
+    tomeData: 'wtf literally. I really want a white peach red beezie rn fuggggggg'
+});
+
+const tome8 = new MoldFlowerTome({
+    tomeName: 'Tome 8 - Cull',
+    tomeData: 'redacted'
+});
+
+const tome9 = new MoldFlowerTome({
+    tomeName: 'Tome 9 - Seen',
+    tomeData: 'aiaiaiaiaiaiia yall ever played the hit game Dead by Daylight? I love when the killer starts FUCKING SLUGGING EVEN THO HE HASNT HOOKED A SINGLE SURVIVOR AND WERE STILL AT 5 GENS'
+});
 
 const moldFlowerTomesArray = [
-    tome1
-]
+    tome1, tome2, tome3,
+    tome4, tome5, tome6, 
+    tome7, tome8, tome9
+];
 console.log(moldFlowerTomesArray)
 
 export { moldFlowerTomesArray };
