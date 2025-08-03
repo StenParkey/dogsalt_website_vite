c431b9201766d763af066ccd0cacaa854fc636de
dogsalt_ <jpstennes@gmail.com>
2025-08-02 18:47:50 -0600

    work on header styling and layout/responsiveness

--- src/components/universal_components/navbar_components/PageHeader.jsx
+++ src/components/universal_components/navbar_components/PageHeader.jsx
@@ -9,23 +9,19 @@ export default function PageHeader({ webpageTitle, webpageSecondaryTitle }) {
         isNavOpen: false,
         isSearchOpen: false,
     });
-    // State to track if the page has been scrolled
     const [isScrolled, setIsScrolled] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
-            // Check if the vertical scroll position is greater than 10 pixels
             setIsScrolled(window.scrollY > 10);
         };
 
-        // Add the scroll event listener when the component mounts
         window.addEventListener('scroll', handleScroll);
 
-        // Remove the event listener when the component unmounts
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
-    }, []); // Empty dependency array ensures the effect runs only once on mount
+    }, []); 
 
     return (
         <header className={`title_section flex_column just_align_center ${isScrolled ? 'scrolled' : ''}`}>
@@ -36,7 +32,9 @@ export default function PageHeader({ webpageTitle, webpageSecondaryTitle }) {
                     {/* Conditionally hide the title block */}
                     {!isScrolled && (
                         <div className='page_title'>
-                            <h1>{webpageTitle}</h1>
+                            <strong>
+                                <h1>{webpageTitle}</h1>
+                            </strong>
                             <h2>{webpageSecondaryTitle}</h2>
                         </div>
                     )}
