dedf0205406450714546f200eabc6fe6019248bd
dogsalt_ <jpstennes@gmail.com>
2025-08-09 11:47:11 -0600

    cleanup

--- src/components/universal_components/navbar_components/PageHeader.jsx
+++ src/components/universal_components/navbar_components/PageHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 import NavButtons from './NavButtons';
 import NavBar from './NavBar';
 import SearchBar from './SearchBar';
