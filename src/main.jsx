281d7265b555b13dd76742e411aa312db4c05fbc
dogsalt_ <jpstennes@gmail.com>
2025-08-10 19:35:31 -0600

    v1 hotfix 2

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_APP_BASE_PATH || '/dogsalt_website_vite'}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
