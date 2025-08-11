6ce67a86431c176e29cf8401f13ca45b7a14a8fc
dogsalt_ <jpstennes@gmail.com>
2025-08-10 19:28:57 -0600

    v1 release fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './main.css';
 
-import './main.css'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.VITE_APP_BASE_PATH || '/dogsalt_website_vite'}>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
